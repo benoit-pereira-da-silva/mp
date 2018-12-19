971224c7c0374ed3f2e6ecf0cd1e02dad563c433
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-12-19 10:11:11 +0100

    Development package configuration

--- Package.swift
+++ Package.swift
@@ -1,24 +1,22 @@
 // swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "mp",
     dependencies: [
-        // You can checkout locally the module to fix multi module issues.
-        // For example
-        //.package(path:"../MPLib"),
+        // You can checkout locally the module to work on multiple modules at the same time.
+        // For example to work on MPLib : .package(path:"../MPLib"),
+        // + swift package generate-xcodeproj
         .package(url:"https://github.com/benoit-pereira-da-silva/CommandLine", from: "4.0.5"),
         .package(url:"https://github.com/benoit-pereira-da-silva/Globals", from: "1.0.0"),
         .package(url:"https://github.com/benoit-pereira-da-silva/Tolerance", from: "1.0.0"),
         .package(url:"https://github.com/benoit-pereira-da-silva/HTTPClient", from: "1.0.0"),
-        .package(url:"https://github.com/benoit-pereira-da-silva/MPLib", from: "1.0.0"),
+        //.package(url:"https://github.com/benoit-pereira-da-silva/MPLib", from: "1.0.0"),
+        .package(path:"../MPLib"),
         .package(url:"https://github.com/benoit-pereira-da-silva/NavetLib", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "mp",
             dependencies: ["CommandLineKit", "Globals","Tolerance", "HTTPClient", "MPLib","NavetLib"]),
