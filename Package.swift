c5640552a7dc9d09f07d0757ab9b5a4fe6cada63
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-12-19 10:05:55 +0100

    Updating the package file

--- Package.swift
+++ Package.swift
@@ -6,12 +6,14 @@ import PackageDescription
 let package = Package(
     name: "mp",
     dependencies: [
+        // You can checkout locally the module to fix multi module issues.
+        // For example
         //.package(path:"../MPLib"),
-        .package(url:"https://github.com/benoit-pereira-da-silva/MPLib", from: "1.0.0"),
         .package(url:"https://github.com/benoit-pereira-da-silva/CommandLine", from: "4.0.5"),
         .package(url:"https://github.com/benoit-pereira-da-silva/Globals", from: "1.0.0"),
         .package(url:"https://github.com/benoit-pereira-da-silva/Tolerance", from: "1.0.0"),
         .package(url:"https://github.com/benoit-pereira-da-silva/HTTPClient", from: "1.0.0"),
+        .package(url:"https://github.com/benoit-pereira-da-silva/MPLib", from: "1.0.0"),
         .package(url:"https://github.com/benoit-pereira-da-silva/NavetLib", from: "1.0.0"),
     ],
     targets: [
@@ -19,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "mp",
-            dependencies: ["MPLib","Globals","Tolerance", "HTTPClient", "CommandLineKit","NavetLib"]),
+            dependencies: ["CommandLineKit", "Globals","Tolerance", "HTTPClient", "MPLib","NavetLib"]),
         .testTarget(
             name: "mpTests",
             dependencies: ["mp"]),
