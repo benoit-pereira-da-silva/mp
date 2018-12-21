0f71e22683b042338a185a9e75971365a1cba597
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-12-21 19:05:16 +0100

    Adding AsyncrhonousOperation package

--- Package.swift
+++ Package.swift
@@ -11,15 +11,16 @@ fileprivate enum Stage {
 fileprivate struct Configuration{
 
     // Turn to .release before to merge on master
-    static let stage:Stage = .release
+    static let stage:Stage = .development
 
     static var dependencies: [PackageDescription.Package.Dependency]{
         switch self.stage{
         case .development:
             return [
                 .package(path:"../CommandLine"),
-                .package(path:"../HMAC"),
                 .package(path:"../Globals"),
+                .package(path:"../HMAC"),
+                .package(path:"../AsynchronousOperation"),
                 .package(path:"../Tolerance"),
                 .package(path:"../HTTPClient"),
                 .package(path:"../MPLib"),
@@ -28,8 +29,9 @@ fileprivate struct Configuration{
         case .release:
             return [
                 .package(url:"https://github.com/benoit-pereira-da-silva/CommandLine", from: "4.0.5"),
-                .package(url:"https://github.com/benoit-pereira-da-silva/HMAC", from: "1.0.0"),
                 .package(url:"https://github.com/benoit-pereira-da-silva/Globals", from: "1.0.0"),
+                .package(url:"https://github.com/benoit-pereira-da-silva/HMAC", from: "1.0.0"),
+                .package(url:"https://github.com/benoit-pereira-da-silva/AsynchronousOperation", from: "1.0.0"),
                 .package(url:"https://github.com/benoit-pereira-da-silva/Tolerance", from: "1.0.0"),
                 .package(url:"https://github.com/benoit-pereira-da-silva/HTTPClient", from: "1.0.0"),
                 .package(url:"https://github.com/benoit-pereira-da-silva/MPLib", from: "1.0.0"),
@@ -47,7 +49,14 @@ let package = Package(
     targets: [
         .target(
             name: "mp",
-            dependencies: ["CommandLineKit","HMAC", "Globals","Tolerance", "HTTPClient", "MPLib","NavetLib"]),
+            dependencies: ["CommandLineKit",
+                           "HMAC",
+                           "Globals",
+                           "AsynchronousOperation",
+                           "Tolerance",
+                           "HTTPClient",
+                           "MPLib",
+                           "NavetLib"]),
         .testTarget(
             name: "mpTests",
             dependencies: ["mp"]),
