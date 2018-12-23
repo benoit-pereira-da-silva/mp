b969b89d08dc6597ee28b5614db2915437021e43
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-12-23 15:30:03 +0100

    Adding an update script

---
+++ core-dev-update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Core devs
+
+cd ../CommandLine
+git pull origin master
+cd ../Globals
+git pull origin master
+cd ../Tolerance
+git pull origin master
+cd ../AsynchronousOperation
+git pull origin master
+cd ../MPLib
+git pull origin master
+cd ../HTTPClient
+git pull origin master
+cd ../NavetLib
+git pull origin master
